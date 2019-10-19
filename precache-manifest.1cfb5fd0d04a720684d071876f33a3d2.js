0df7e3509945c2b916361403af3eb0ac403f28ab
Jose <jose@Joses-MacBook-Pro.local>
2019-10-19 23:50:54 +0200

    Updates

---
+++ precache-manifest.1cfb5fd0d04a720684d071876f33a3d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31763d5188a1ad0f75dc2135981200eb",
+    "url": "/vts/index.html"
+  },
+  {
+    "revision": "888df2ea7cf382cfc4aa",
+    "url": "/vts/static/css/main.4db7a6e5.chunk.css"
+  },
+  {
+    "revision": "781a9bdcd6ff836a370c",
+    "url": "/vts/static/js/2.5e7aa6ad.chunk.js"
+  },
+  {
+    "revision": "888df2ea7cf382cfc4aa",
+    "url": "/vts/static/js/main.94eaf068.chunk.js"
+  },
+  {
+    "revision": "35ced0d9b941fb76c595",
+    "url": "/vts/static/js/runtime-main.09974ec5.js"
+  },
+  {
+    "revision": "d2eb83ae47d2f1c009e25fb83363836a",
+    "url": "/vts/static/media/vts.d2eb83ae.csv"
+  }
+]);
